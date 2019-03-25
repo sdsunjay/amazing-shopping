348775cc40ffa14a8f8634b8c8fc8686ff73e277
Sunjay <sdsunjay73@yahoo.com>
2019-03-24 23:06:51 -0400

    include pagy and set_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  def set_user
+    return @user if defined? @user
+
+    @user = current_user
+  end
 end

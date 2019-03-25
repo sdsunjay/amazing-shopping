ab5d849c8596243b4dbff4cb05bc95d0d647fc3b
Sunjay <sdsunjay73@yahoo.com>
2019-03-24 23:14:13 -0400

    Routes for users, products, orders, and inventories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    resources :users, only: [:show]
+    authenticated :user do
+      resources :products, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+      resources :orders, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+      resources :inventories, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+     end
+  end
+  root to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

f698033cfaddf6c37a048113aace6f3e6a9daec9
Sunjay <sdsunjay73@yahoo.com>
2019-03-28 16:38:20 -0400

    possible future adding of admin users

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
       resources :orders, only: [:new, :create, :index, :show, :edit, :destroy, :update]
       resources :inventories, only: [:new, :create, :index, :show, :edit, :destroy, :update]
      end
+    # authenticate :user, ->(user) { user.super_admin? } do
+    #  resources :inventories, only: [:edit, :update, :destroy]
+    # end
   end
   root to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
